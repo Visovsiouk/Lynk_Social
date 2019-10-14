f354fd609f4f81797711b7cf9acb5e1ad41e8e5d
Steve Popoola <stevepop@gmail.com>
2019-10-14 12:36:46 +0100

    Separate bootstrap in webpack config  into vendor bundle to seprate from VueJS bundle Installed VueJS

---
+++ resources/js/components/PostForm.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Post Form goes here
+  </div>
+</template>
+
+<script>
+  export default {
+    props:[],
+    data () {
+      return {
+  
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -1,11 +1,7 @@
 @extends('layout.app')
 @section('content')
 @include("includes/_navigation")
-<style>
-    body {
-        background-color: #edf2f7;
-    }
-</style>
+
 <div class="alert alert-message m-0 border-0" role="alert">
 
     <h6 class="text-center text-light pt-2">Help make Lynk, Discover how you can help build <u style="color:#4FD1C5;">here on github</u></h6>
